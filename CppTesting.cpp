3aab0f53d096ea774ee0b74ccf2d03d7900418a3
Ben <benjaminehuddle@gmail.com>
2019-01-06 20:34:49 -0800

    testing memory of loops

--- CppTesting.cpp
+++ CppTesting.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 
 int main() {
-    cout << "hello world" << endl;
+    int counter = 0;
+    
+    // for loop
+    for(counter = 0; counter < 10; ++counter) {
+        //this is for counting to ten
+        cout << "The counter is: " << counter << endl;
+    };
+    
+    //reset counter to 0 and while loop
+    counter = 0;
+    while (true) {
+        cout << "While loop counter is: " << counter << endl;
+        counter++;
+        if (counter < 10) {
+            break;
+        }
+    };
+    
+    // do while loop
+    counter = 0;
+    do {
+        cout << "Do while loop counter is: " << counter << endl;
+        counter++;
+    } while (counter < 10);
+    
+    cout << "All loops passed" << endl;
+    
     return 0;
 }
