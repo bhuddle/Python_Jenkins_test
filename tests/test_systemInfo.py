3a9d19796b7e39490fa0530ff97b6b0ef5900a89
Ben <benjaminehuddle@gmail.com>
2019-01-10 07:50:44 -0800

    had issues with variables in pytest

--- tests/test_systemInfo.py
+++ tests/test_systemInfo.py
@@ -1,2 +1,19 @@
+import subprocess, pytest
 #test get system info and check versions
-def test_systemInfo():
+    
+def test_getInfo():
+    sysInfo = subprocess.Popen('systeminfo', stdout=subprocess.PIPE).communicate()[0].split('\n')
+    for x in sysInfo:
+        if 'Host Name' in x:
+            hostname = x
+        if 'OS Version' in x and 'BIOS' not in x:
+            osver = x
+        if 'BIOS Version' in x:
+            biosver = x
+        if 'Owner' in x:
+            owner = x
+
+    assert 'BENDERRODRIGUEZ' in hostname
+    assert '10.0.17134 N/A Build 17134' in osver
+    assert 'American Megatrends Inc. 4101   , 4/13/2011' in biosver
+    assert 'Bender' in owner
